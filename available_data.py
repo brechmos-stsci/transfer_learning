32dd251707ce18d00d1bb74f4a0a9cf13bb2b97c
Craig Jones <craig@brechmos.org>
2017-11-29 17:07:14 +0000

    fixed image rescaling

--- available_data.py
+++ available_data.py
@@ -17,13 +17,11 @@ async def get_data(id):
 
     log.info('Getting data for id {}'.format(id))
 
-    to_send = np.zeros((224,224,4))
-    to_send[:,:,:3] = np.load('data/image_{}.npy'.format(id))
+    # rgb + alpha channel
+    data = np.load('data/carina/image_{}.npy'.format(id))
 
     # rescale to be between 0 and 255
-    cmin, cmax = np.percentile(to_send, [3, 97])
-    to_send = np.clip((to_send - cmin) / (cmax-cmin) * 255, 0, 255)
-    to_send[:,:,3] = 255*np.ones((224,224))
+    to_send = normalize_rgb(data, 3, 97)
 
     return {
         'rgb': True,
@@ -32,3 +30,22 @@ async def get_data(id):
         'values': to_send.transpose((2,0,1)).ravel(order='F').tolist(),
     }
 
+
+def normalize_rgb(data, lower_percentile=3, upper_percentile=97):
+    """
+    Normalize the RGB data to be between 0 and 255 and also add 
+    an alpha channel 
+    """
+
+    nrows, ncols = data.shape[:2]
+
+    data_out = np.zeros((nrows, ncols, 4))
+
+    # rescale to be between 0 and 255
+    cmin, cmax = np.percentile(data, [lower_percentile, upper_percentile])
+    data_out[:,:,:3] = np.clip((data - cmin) / (cmax-cmin) * 255, 0, 255)
+
+    # Add Alpha channel
+    data_out[:,:,3] = 255*np.ones((nrows, ncols))
+
+    return data_out
