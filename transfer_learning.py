fc3ced18349cb672602a347a74132eb2777b3354
Craig Jones <crjones@stsci.edu>
2017-11-29 20:23:21 -0500

    pep8

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,14 +1,15 @@
 from quart import render_template, jsonify, Blueprint, current_app
 import json
 from available_data import get_data
+import transfer_learning_queries as tlqueries
 
 import logging
 logging.basicConfig()
 log = logging.getLogger('hdltl')
 
-import transfer_learning_queries as tlqueries
 
 blueprint = Blueprint('transfer_learning', __name__)
+
 
 @blueprint.route('/transfer_learning')
 async def transfer_learning():
@@ -30,7 +31,6 @@ async def getcutout(slice, similarity_type):
     inds = []
     similarity_values = []
 
-
     if similarity_type in ['tsne', 'tanimoto', 'norm1', 'inf_norm', 'cosine_similarity']:
 
         # Get the query
@@ -39,10 +39,13 @@ async def getcutout(slice, similarity_type):
         # Get the tsne value for the main image from the database
         async with current_app.pool.acquire() as connection:
             async with connection.transaction():
-                row = await connection.fetchrow("""SELECT fingerprint FROM test WHERE fingerprint_type='tsne' and image_id=$1;""", slice)
+                row = await connection.fetchrow("""
+                    SELECT fingerprint 
+                    FROM test 
+                    WHERE fingerprint_type='tsne' and image_id=$1;
+                    """, slice)
                 tsne_value = json.loads(row['fingerprint'])['values']
 
-    
         # Find the similar images
         query = query.format(tsne_value[0], tsne_value[1])
         async with current_app.pool.acquire() as connection:
@@ -50,8 +53,6 @@ async def getcutout(slice, similarity_type):
                 async for row in connection.cursor(query):
                     inds.append(row[0]) 
                     similarity_values.append(row[1]) 
-
-
 
     elif similarity_type == 'jaccard':
 
@@ -62,7 +63,11 @@ async def getcutout(slice, similarity_type):
         async with current_app.pool.acquire() as connection:
             async with connection.transaction():
                 # Get the tsne value from the database
-                row = await connection.fetchrow("""SELECT fingerprint FROM test WHERE fingerprint_type='fingerprints' and image_id={};""".format(slice))
+                row = await connection.fetchrow("""
+                    SELECT fingerprint 
+                    FROM test 
+                    WHERE fingerprint_type='fingerprints' and image_id={};
+                    """.format(slice))
                 labels = json.loads(row[0])['labels']
         
         query = query.format(json.dumps(labels))
@@ -72,8 +77,7 @@ async def getcutout(slice, similarity_type):
             async with connection.transaction():
                 async for row in connection.cursor(query):
                     inds.append(row[0]) 
-                    similarity_values.append(row[1]) 
-
+                    similarity_values.append(row[1])
 
     elif similarity_type == 'none':
         # This is to allow getting of data without having to calculate the information
