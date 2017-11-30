4debc7dc56566360ef7eab9bf6005f8610b299d7
Craig Jones <crjones@stsci.edu>
2017-11-29 20:21:25 -0500

    pep8

--- hdltl.py
+++ hdltl.py
@@ -2,8 +2,8 @@ from quart import render_template, Blueprint
 
 blueprint = Blueprint('index', __name__)
 
+
 @blueprint.route('/')
 @blueprint.route('/index')
 async def index():
     return await render_template('index.html', title='Home')
-
