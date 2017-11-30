f5fda1d87274f3a6476f0944bd04128b4ffd7595
Craig Jones <craig@brechmos.org>
2017-11-29 20:20:44 -0500

    pep8

--- available_data.py
+++ available_data.py
@@ -7,13 +7,14 @@ log = logging.getLogger('hdltl')
 
 blueprint = Blueprint('available_data', __name__)
 
-@blueprint.route('/data/<int:id>/')
-async def get_data(id):
 
-    log.info('Getting data for id {}'.format(id))
+@blueprint.route('/data/<int:id>/')
+async def get_data(data_id):
+
+    log.info('Getting data for id {}'.format(data_id))
 
     # rgb + alpha channel
-    data = np.load('data/carina/image_{}.npy'.format(id))
+    data = np.load('data/carina/image_{}.npy'.format(data_id))
 
     # rescale to be between 0 and 255
     to_send = normalize_rgb(data, 3, 97)
@@ -22,7 +23,7 @@ async def get_data(id):
         'rgb': True,
         'width': to_send.shape[0], 
         'height': to_send.shape[1], 
-        'values': to_send.transpose((2,0,1)).ravel(order='F').tolist(),
+        'values': to_send.transpose((2, 0, 1)).ravel(order='F').tolist(),
     }
 
 
@@ -38,7 +39,7 @@ def normalize_rgb(data, lower_percentile=3, upper_percentile=97):
 
     # rescale to be between 0 and 255
     cmin, cmax = np.percentile(data, [lower_percentile, upper_percentile])
-    data_out[:,:,:3] = np.clip((data - cmin) / (cmax-cmin) * 255, 0, 255)
+    data_out[:, :, :3] = np.clip((data - cmin) / (cmax-cmin) * 255, 0, 255)
 
     # Add Alpha channel
     data_out[:, :, 3] = 255*np.ones((nrows, ncols))
